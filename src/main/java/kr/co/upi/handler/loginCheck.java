142d3f45369e1092d7f19f61766f5ee0e025378a
kwackjiyong <rhkrwldyd@naver.com>
2020-07-31 16:32:40 +0900

    세션체크

---
+++ src/main/java/kr/co/upi/handler/loginCheck.java
@@ -0,0 +1,46 @@
+package kr.co.upi.handler;
+
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import kr.co.upi.DTO.UserDTO;
+
+public class loginCheck extends HandlerInterceptorAdapter{
+	@Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+		
+        return true;
+    }
+ 
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+            ModelAndView modelAndView) throws Exception {
+        // TODO Auto-generated method stub
+    	response.setContentType("text/html; charset=UTF-8");
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+        HttpSession session = request.getSession();
+        String userid = (String) session.getAttribute("USER_ID");
+        
+    	if(userid == null){
+            
+    		modelAndView.setViewName("user/login");
+            
+        }
+        super.postHandle(request, response, handler, modelAndView);
+    }
+    
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+            throws Exception {
+        // TODO Auto-generated method stub
+        super.afterCompletion(request, response, handler, ex);
+    }
+}
